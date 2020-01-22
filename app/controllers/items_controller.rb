9864564409adf2dd83f1f5307a3e61d973d93cab
br-to <barabara0224@gmail.com>
2020-01-22 15:05:59 +0900

    add item controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::ItemsController < ApplicationController
+  before_action :set_item, only: [ :show, :update, :destroy ]
+
+  def index
+    @items = Item.all
+    render json: @items
+  end
+
+  def show
+    render json: @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      @item.parse_base64(params[:image])
+      render json: @item, status: :created
+    else
+      redner json: @item.error, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.error, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image)
+  end
+end
