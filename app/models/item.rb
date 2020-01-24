5c5a24d1b4be4134abd0bb20d1d6c0486093e010
br-to <barabara0224@gmail.com>
2020-01-24 22:53:43 +0900

    fix parse_base64

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,15 @@ class Item < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true
-  has_one_attached :image
+  has_one_attached :item_image
+  attr_accessor :image
+
+  def parse_base64
+    if image.present?
+      content = image.split(',')[1]
+      filename = Time.zone.now.to_s + '.png'
+      decoded_data = Base64.decode64(content)
+      item_image.attach(io: StringIO.new(decoded_data), filename: filename)
+    end
+  end
 end
