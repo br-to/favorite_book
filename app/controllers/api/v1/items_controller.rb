81d0ec40828ac1208bd835fcf9bb0dbdde1cec22
br-to <barabara0224@gmail.com>
2020-01-27 16:11:38 +0900

    Remove instance variables

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,40 +2,40 @@ class Api::V1::ItemsController < ApplicationController
   before_action :set_item, only: [ :show, :update, :destroy ]
 
   def index
-    @items = Item.where("title = ?", params[:title])
-    render json: @items
+    items = Item.where("title = ?", params[:title])
+    render json: items
   end
 
   def show
-    render json: @item
+    render json: item
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      @item.parse_base64
-      render json: @item, status: :created
+    item = Item.new(item_params)
+    if item.save
+      item.parse_base64
+      render json: item, status: :created
     else
-      render json: @item.errors, status: :unprocessable_entity
+      render json: item.errors, status: :unprocessable_entity
     end
   end
   
   def update
-    if @item.update(item_params)
-      render json: @item
+    if item.update(item_params)
+      render json: item
     else
-      render json: @item.errors, status: :unprocessable_entity
+      render json: item.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @item.destroy
+    item.destroy
   end
 
   private
 
   def set_item
-    @item = Item.find(params[:id])
+    item = Item.find(params[:id])
   end
 
   def item_params
