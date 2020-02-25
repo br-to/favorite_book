7d193543bcf69a6d7872096198752085c7655f93
br-to <barabara0224@gmail.com>
2020-02-25 11:22:21 +0900

    認証機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    if request.headers['Authorization'].present?
+      begin
+      encode_token = request.headers['Authorization'].split('Bearer ').last
+      @decode_token = JWT.decode(encode_token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')[0] 
+      rescue JWT::DecodeError, JWT::VerificationError
+        render json: { errors: ['Missing credentials'] }, status: :unauthorized
+      end
+    else
+      render json: { errors: ['Missing token'] }, status: :unauthorized 
+    end
+  end
 end
