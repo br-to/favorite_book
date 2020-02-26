ea492b2c9376019d2d54bfbb64763b874c3a7e39
br-to <barabara0224@gmail.com>
2020-02-26 12:56:24 +0900

    ロード時にERBをかませる

--- config/initializers/00_load_config.rb
+++ config/initializers/00_load_config.rb
@@ -1,3 +1,3 @@
 file = "#{Rails.root}/config/secrets.yml"
 
-SECRET_KEY = YAML.load_file(file)[Rails.env].deep_symbolize_keys
+SECRET_KEY =  YAML.load(ERB.new(File.read(file)).result)[Rails.env].deep_symbolize_keys
